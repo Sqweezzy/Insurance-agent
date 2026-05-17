ba75fd3f2e353fb15e968cd9bd5a116492557f43
Sqweezzy <kirook223@gmail.com>
2026-05-17 16:35:10 +0500

    init

---
+++ main/settings.py
@@ -0,0 +1,201 @@
+# Содержит основные настройки django проекта
+# конфигурация приложений, шаблонов, middleware...
+
+import os
+from pathlib import Path
+from decouple import config
+
+# Базовая директория проекта
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Секретный ключ для безопасности
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+
+
+# Список встроенных django приложений
+DJANGO_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+# Список сторонних приложений
+THIRD_PARTY_APPS = [
+    'rest_framework',
+    'corsheaders',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist'
+]
+
+# Список локальных приложений
+LOCAL_APPS = [
+    'apps.accounts'
+]
+
+# Общий список
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+# Список middlewware для обработки запросов
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Корневой url файл проекта
+ROOT_URLCONF = 'config.urls'
+
+# Конфигурация шаблонов django 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# WSGI_APPLICATION = 'notes_project.wsgi.application'
+
+# Конфигурация БД
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB', default='modelhub'),
+        'USER': config('POSTGRES_USER', default='postgres'),
+        'PASSWORD': config('POSTRGES_PASSWORD', default='password'),
+        'HOST': config('POSTGRES_HOST', default='localhost'),
+        'PORT': config('POSTGRES_PORT', default='5432'),
+        'ATOMIC_REQUESTS': True,
+    }
+}
+
+
+#Валидаторы паролей
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Настройка интернациональности
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+
+# Настройка статических файлов
+STATIC_URL = 'static/' # Url статики
+STATIC_ROOT = BASE_DIR / config('STATIC_ROOT', default='staticfiles') # Путь для собранных файлов статики
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / config('MEDIA_ROOT', default='media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+
+# Настройки django REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Разрешить доступ только аутентифицированным пользователям.
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',  # Ограничение запросов для анонимных пользователей
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/hour',  # Лимит запросов для анонимных пользователей
+    },
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',  # Рендеринг в JSON
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',  # Парсинг JSON-данных
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+# Настройка CORS для разработки и продакшена
+if DEBUG:
+    CORS_ALLOW_ALL_ORIGINS = True
+else:
+    CORS_ALLOWED_ORIGINS = [ # Разрешенные источники
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'http://localhost:8000',
+        'http://127.0.0.1:8000'
+    ]
+
+# JWT Configuration
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+}
+
+# Настройки безопасности
+SECURE_BROWSER_XSS_FILTER = True  # Защита от XSS-атак
+SECURE_CONTENT_TYPE_NOSNIFF = True  # Запрет MIME-типов
+X_FRAME_OPTIONS = 'DENY'  # Защита от кликджекинга
+
+# Настройки логирования
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',  # Уровень логирования
+            'class': 'logging.FileHandler',  # Логирование в файл
+            'filename': BASE_DIR / 'debug.log',  # Путь к файлу логов
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],  # Используемый обработчик
+            'level': 'INFO',  # Уровень логирования
+            'propagate': True,  # Передача логов родительским логгерам
+        },
+    },
+}
+
+os.makedirs(BASE_DIR / 'logs', exist_ok=True)
